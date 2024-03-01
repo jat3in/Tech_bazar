8b878d4a401731ad475911297b2164ee640db1b7
Jatin <jatinvashishtha053@gmail.com>
2024-03-01 14:16:17 +0530

    cors and cookieParse set

--- Tech_Bazar_Backend/src/index.js
+++ Tech_Bazar_Backend/src/index.js
@@ -3,7 +3,15 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
 import { DB_NAME } from './constant.js'
-
+import { app } from './app.js'
 dotenv.config()
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is listing on ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+})
